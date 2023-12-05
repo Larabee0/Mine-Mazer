bd29dcb0943b344b584b49d667e588f69d1f47a5
RHYS RICHARDS <RR278256@falmouth.ac.uk>
2023-12-05 14:29:45 +0000

    Fungus activates when Interacting with NPC.

--- Assets/Scripts/Interact_Example.cs
+++ Assets/Scripts/Interact_Example.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using Fungus;
 
 public class Interact_Example : MonoBehaviour, IInteractable
 {
+    public Flowchart Dialogue;
     // Start is called before the first frame update
     public void Interact()
     {
-        Debug.Log("Testing. Testing. 1 2 3.");
+        Dialogue.ExecuteBlock("NPC Interact");
     }
 
     // Update is called once per frame
