4d4cf9c71f951b7db77992e5f2ef12ccd1d045e0
William Vickers Hastings <WV276829@falmouth.ac.uk>
2024-03-08 15:47:24 +0000

    Fixed nav mesh errors when you pick up eudie

--- Assets/Scripts/Lumenite_AI/Follow_Player.cs
+++ Assets/Scripts/Lumenite_AI/Follow_Player.cs
@@ -6,15 +6,22 @@ using UnityEngine.AI;
 public class Follow_Player : MonoBehaviour
 {
     private GameObject player;
+    private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = player.transform.position;
+
+        if (agent.enabled)
+        {
+            agent.destination = player.transform.position;
+        }
     }
 }
